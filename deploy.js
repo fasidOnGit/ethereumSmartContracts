f1b18d4ae211e1c13e88442c4ed25776dd7e533a
Kader Mohideen Fasid <Fasid@Fasid.local>
2018-04-29 23:28:03 +0530

    Done with deployment

---
+++ deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+
+const provider = new HDWalletProvider(
+    'word win world fall valid field awkward tool bridge knee blush afford',
+    'https://mainnet.infura.io/L40Is7cV2Vr6TCf0ZCoI'
+    // 'https://rinkeby.infura.io/L40Is7cV2Vr6TCf0ZCoI'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments:['Hi There!']})
+        .send({ gas: '1000000', from: accounts[0]});
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
